7f04f472487c06af96d2c44654dc020d28f3a3e5
hz <huizhantan@gmail.com>
2022-11-19 20:02:57 +0800

    feat: finished text field controls

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/pages/learn_flutter_page.dart';
 import 'package:learn_flutter/pages/simple_form_page.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,32 +6,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      heightFactor: 20,
-        child: Column(
-      children: [
-        const SizedBox(height: 12),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (BuildContext context) {
-                return const LearnFlutterPage();
-              }),
-            );
-          },
-          child: const Text('Learn Flutter'),
-        ),
-        const SizedBox(height: 12),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (BuildContext context) {
-                  return const SimpleForm();
-                }),
-              );
-            },
-            child: const Text('Stenbok Simple Form Test'))
-      ],
-    ));
+    return const Center(
+        child: SimpleForm(),
+        // child: Column(
+      // children: const [
+        // const SizedBox(height: 12),
+        // ElevatedButton(
+        //   onPressed: () {
+        //     Navigator.of(context).push(
+        //       MaterialPageRoute(builder: (BuildContext context) {
+        //         return const LearnFlutterPage();
+        //       }),
+        //     );
+        //   },
+        //   child: const Text('Learn Flutter'),
+        // ),
+        // ElevatedButton(
+        //     onPressed: () {
+        //       Navigator.of(context).push(
+        //         MaterialPageRoute(builder: (BuildContext context) {
+        //           return const SimpleForm();
+        //         }),
+        //       );
+        //     },
+        //     child: const Text('Stenbok Simple Form Test'))
+      // ],
+    // ),
+    );
   }
 }
--- lib/pages/simple_form_page.dart
+++ lib/pages/simple_form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SimpleForm extends StatefulWidget {
   const SimpleForm({super.key});
@@ -8,8 +9,122 @@ class SimpleForm extends StatefulWidget {
 }
 
 class _SimpleFormState extends State<SimpleForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _validateEmail(String? value) {
+    String pattern =
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?)*$";
+    RegExp regex = RegExp(pattern);
+    if (value == null || value.isEmpty || !regex.hasMatch(value)) {
+      return 'Enter a valid email address';
+    } else {
+      return null;
+    }
+  }
+
+  void _submitForm() => {
+        if (_formKey.currentState!.validate())
+          {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Processing Data')),
+            )
+          }
+      };
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Simple Form"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(80.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Flexible(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "First Name",
+                        helperText: " ",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 40),
+                  Expanded(
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Last Name",
+                        helperText: " ",
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            // ignore: prefer_const_constructors
+            Flexible(
+              child: const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Small Description',
+                    helperText: " ",
+                    alignLabelWithHint: true),
+                keyboardType: TextInputType.multiline,
+                maxLines: 5,
+              ),
+            ),
+            const SizedBox(height: 24),
+            Flexible(
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email",
+                  helperText: " ",
+                ),
+                validator: (value) => _validateEmail(value),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () => _submitForm(),
+              child: const Text('Submit'),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
